5d375df2f937dd232deee72dbe8e95ec1f316a9a
Melissa McEwen <melissa@pika.dev>
2020-10-05 15:06:11 -0500

    my little bug

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,13 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import helpers from 'helpers';
+
 interface AppProps {}
 
 function App({}: AppProps) {
-  const [count, setCount] = useState(0);
-  setTimeout(() => setCount(count + 1), 1000);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+  console.log(helpers);
+  return null;
 }
 
 export default App;
